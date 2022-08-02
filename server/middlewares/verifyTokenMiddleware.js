842b88d684c9ca21ce4f0fd328ca966c864d858c
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-02 14:46:55 +0300

    Implement verifyTokenMiddleware

---
+++ server/middlewares/verifyTokenMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+
+const verifyTokenMiddleware = (req, res, next) => {
+  const token = req.headers["authorization"]
+
+  if (!token) {
+    return res.status(403).send("No token provided")
+  }
+
+  try {
+    req.user = jwt.decode(token, process.env.TOKEN)
+  } catch (e) {
+    return res.status(401).send("Invalid token")
+  }
+
+  return next()
+}
+
+module.exports = verifyTokenMiddleware
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 require("express-group-routes")
 const express = require("express")
 const router = express()
+const verifyTokenMiddleware = require("../middlewares/verifyTokenMiddleware")
 const UserController = require("../controllers/userController")
+
+router.use(verifyTokenMiddleware)
 
 router.group("/users", (router) => {
   router.get("/", UserController.index)
