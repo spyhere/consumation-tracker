cea583d7055bfac68fe84c6289f9002dd65ee774
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-02 15:34:31 +0300

    Implement entryController index method

---
+++ server/controllers/entryController.js
@@ -0,0 +1,42 @@
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient()
+
+class EntryController {
+
+  static async index(req, res) {
+    const cursor = req.query.cursor ? {
+      skip: 1,
+      cursor: {
+        id: Number(req.query.cursor)
+      }
+    } : {}
+
+    const userId = req.user.id
+    const entries = await prisma.entry.findMany({
+      take: 5,
+      ...cursor,
+      where: {
+        user_id: userId
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+    res.send({ entries })
+  }
+
+  static async store(req, res) {
+    //
+  }
+
+  static async update(req, res) {
+    //
+  }
+
+  static async destroy(req, res) {
+    //
+  }
+}
+
+module.exports = EntryController
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,17 @@ const router = express()
 const verifyTokenMiddleware = require("../middlewares/verifyTokenMiddleware")
 const isAdminMiddleware = require("../middlewares/isAdminMiddleware")
 const UserController = require("../controllers/userController")
+const EntryController = require("../controllers/entryController")
 
 router.use(verifyTokenMiddleware)
 
 router.group("/users", (router) => {
   router.use(isAdminMiddleware)
   router.get("/", UserController.index)
+})
+
+router.group("/entries", (router) => {
+  router.get("/", EntryController.index)
 })
 
 module.exports = router
