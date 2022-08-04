3c67727d7bb2fd68a5042bd2728ae83f31d8cd2d
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-04 17:21:48 +0300

    Implement AdminGuard

---
+++ client/src/guards/AdminGuard.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navigate, useLocation } from "react-router-dom"
+import { AuthEnum } from "../enums"
+import { useAuthContext } from "../contexts/AuthContext"
+
+type Props = {
+  children: JSX.Element
+}
+
+const AdminGuard = ({ children }: Props) => {
+  const { role } = useAuthContext()
+  const location = useLocation()
+
+  if (role !== AuthEnum.ADMIN) {
+    return <Navigate to="/entries" state={{ from: location }} replace/>
+  }
+
+  return children
+}
+
+
+export default AdminGuard
