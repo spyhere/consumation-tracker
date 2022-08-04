39175cd6027d5013e00d27d1ba864767c0c2ca4e
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-04 23:49:54 +0300

    Implement axios apiClient

---
+++ client/src/api/apiClient.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosRequestConfig } from "axios"
+import { USER_TOKEN } from "../resources/constants"
+
+
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  }
+})
+
+function requestInterceptor(config: AxiosRequestConfig) {
+  const token = localStorage.getItem(USER_TOKEN)
+  if (token && config.headers) {
+    config.headers['Authorization'] = token
+  }
+  return config
+}
+
+apiClient.interceptors.request.use(requestInterceptor, (error) => Promise.reject(error))
+
+export default apiClient
