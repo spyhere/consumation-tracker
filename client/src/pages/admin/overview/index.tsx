225476387efbff3731ace1971a82619edd85441a
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-06 00:04:06 +0300

    Add admin overview page

--- client/src/pages/admin/overview/index.tsx
+++ client/src/pages/admin/overview/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import {
+  Link,
+  useLocation
+} from "react-router-dom"
+import {
+  Col,
+  Layout,
+  PageHeader,
+  Row,
+  Spin,
+  Typography
+} from "antd"
+import { useUsersStats } from "queries/user"
+import UserCard from "components/UserCard"
 
-const Overview = () => {
 
-  return <>Overview</>
+const AdminOverview = () => {
+  const { pathname } = useLocation()
+  const { data: usersData, isLoading: isLoadingUsers } = useUsersStats()
+
+  const { data: users } = usersData || {}
+
+  return (
+    <Spin spinning={isLoadingUsers}>
+      <PageHeader>
+        <Typography.Title level={3}>Users Overview</Typography.Title>
+      </PageHeader>
+      <Layout>
+        <Layout.Content>
+          <Row gutter={[16, 16]}>
+            {users?.map(it => (
+              <Col span={8} key={it.id}>
+                <Link to={`${pathname}/users/${it.id}`}>
+                  <UserCard user={it} key={it.id}/>
+                </Link>
+              </Col>
+            ))}
+          </Row>
+        </Layout.Content>
+      </Layout>
+    </Spin>
+  )
 }
 
 
-
-export default Overview
+export default AdminOverview
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { Route, Routes } from "react-router-dom"
+import {
+  Route,
+  Routes
+} from "react-router-dom"
 import AdminGuard from "guards/AdminGuard"
-import Overview from "pages/admin/overview"
+import AdminOverview from "pages/admin/overview"
 import Entries from "pages/user/entries"
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="admin">
-        <Route path="overview" element={<AdminGuard><Overview /></AdminGuard>} />
+        <Route path="overview">
+          <Route path="" element={<AdminGuard><AdminOverview/></AdminGuard>}/>
+          <Route path="users/:id" element={<AdminGuard><Entries/></AdminGuard>}/>
+        </Route>
       </Route>
 
       <Route path="">
-        <Route path="" element={<Entries />} />
+        <Route path="" element={<Entries/>}/>
       </Route>
     </Routes>
   )
