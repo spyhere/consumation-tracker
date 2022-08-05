ac81f69598fef31ae79232fa94c28cc28bb5cf2d
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-05 23:32:04 +0300

    Add `T` to every type in entryService.ts

--- client/src/api/entry/entryService.ts
+++ client/src/api/entry/entryService.ts
@@ -2,7 +2,7 @@ import apiClient from "../apiClient"
 import { DayT } from "./types"
 import { QueryFunctionContext } from "@tanstack/react-query"
 
-export type entriesPaginated = {
+export type entriesPaginatedT = {
   data: {
     dates: DayT[]
   }
@@ -10,17 +10,17 @@ export type entriesPaginated = {
     cursor: number
   }
 }
-export const getPaginatedEntries = ({ pageParam }: QueryFunctionContext): Promise<entriesPaginated> => {
+export const getPaginatedEntries = ({ pageParam }: QueryFunctionContext): Promise<entriesPaginatedT> => {
   return apiClient.get(`/entries?cursor=${pageParam || ""}`)
     .then((response) => response.data)
 }
 
 
-export type entriesStats = {
+export type entriesStatsT = {
   monthMoneySpent: string,
   dayCalories: number | null
 }
-export const getEntriesStats = (): Promise<entriesStats> => {
+export const getEntriesStats = (): Promise<entriesStatsT> => {
   return apiClient.get("/entries/stats").then((response) => response.data.data)
 }
 
