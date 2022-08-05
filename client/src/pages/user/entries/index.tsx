ed068613a6826b3018414e8d15b41b8f721d69d5
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-05 18:30:12 +0300

    Render entries by days in entries page

--- client/src/pages/user/entries/index.tsx
+++ client/src/pages/user/entries/index.tsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import DaysOverview from "components/DaysOverview"
+import {
+  useEntriesPaginated,
+  useEntriesStats
+} from "queries/entry"
+import {
+  DollarOutlined,
+  DotChartOutlined
+} from "@ant-design/icons"
+import {
+  Col,
+  Divider,
+  Layout,
+  PageHeader,
+  Row,
+  Statistic,
+  Typography
+} from "antd"
+
+const { Header, Content } = Layout
+const { Title } = Typography
 
 const Entries = () => {
-  return <>Entries</>
-}
+  const { data: { data, meta } = {} } = useEntriesPaginated()
+  const { data: { monthMoneySpent, dayCalories } = { monthMoneySpent: "", dayCalories: null } } = useEntriesStats()
 
+  const loadMoreDates = () => {
+    console.log("load more")
+  }
+
+  return (
+    <>
+      <PageHeader>
+        <Title level={3}>Entries</Title>
+      </PageHeader>
+      <Layout>
+        <Header>
+          <Row justify="center" gutter={16}>
+            <Col span={4}>
+              <Statistic title="Money spent this month" value={monthMoneySpent} prefix={<DollarOutlined/>}/>
+            </Col>
+            <Col span={4}>
+              <Statistic title="Calories consumed this day" value={dayCalories || 0} prefix={<DotChartOutlined/>}/>
+            </Col>
+          </Row>
+        </Header>
+        <Content>
+          <Divider/>
+          {data?.dates && meta && (
+            <DaysOverview data={data} meta={meta} loaderFunction={loadMoreDates}/>
+          )}
+        </Content>
+      </Layout>
+    </>
+  )
+}
 
 
 export default Entries
