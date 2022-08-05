55397f42b52b195735d0991a7d656e6d2a9651a6
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-05 22:15:50 +0300

    Add marker for today day

--- client/src/components/Day.tsx
+++ client/src/components/Day.tsx
@@ -14,14 +14,16 @@ type Props = {
   data: DayT
 }
 
+const today = new Date().toISOString().split("T")[0]
 
 const Day = ({ data }: Props) => {
   const calLimitReached = data.consumed >= Number(CALORIES_LIMIT)
 
   const dangerStyle = calLimitReached ? { background: "#ff7875", color: "#fff" } : {}
+  const isToday = (data.daytime === today) ? "Today " : ""
 
   return (
-    <Card title={<CardWarningTitle title={data.daytime} isWarning={calLimitReached}/>} hoverable
+    <Card title={<CardWarningTitle title={isToday + data.daytime} isWarning={calLimitReached}/>} hoverable
           headStyle={dangerStyle}>
       <b>Consumed: </b><code>{data.consumed}</code>
       {' '}
