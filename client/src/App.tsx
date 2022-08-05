0cf902278da500023c6faf3d0d2760ed9438b703
Eugene-Ponomarov <gekav3@mail.ru>
2022-08-05 20:24:25 +0300

    Move JWT token to constants.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import AppRoutes from "./routes"
-import { USER_TOKEN } from "resources/constants"
+import {
+  JWT_TOKEN,
+  USER_TOKEN
+} from "resources/constants"
 import { AuthContextProvider } from "./contexts/AuthContext"
 
 function App() {
-  localStorage.setItem(USER_TOKEN, process.env.REACT_APP_USER_TOKEN || "")
+  localStorage.setItem(USER_TOKEN, JWT_TOKEN)
 
   return (
     <>
--- client/src/resources/constants.ts
+++ client/src/resources/constants.ts
@@ -8,6 +8,10 @@ export const QUERY_OPTIONS = {
   }
 }
 
+// Customer
+export const JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkFsaWNlIiwicm9sZSI6IkNVU1RPTUVSIiwiaWF0IjoxNjU5NDM5OTYxfQ.io_ziy3PlypMofipMuc6UFyU_pCK4NgCR4V6_VS0Ca0"
+// Admin
+// export const JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6IkJvYiIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTY1OTQ0MDAxMX0.j1jXq-DPqrrR_nQI60k79DVSKk_IHFFi_rQEXG0JsmU"
 export const USER_TOKEN = "USER_TOKEN"
 
 export const CALORIES_LIMIT = 500
